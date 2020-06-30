b75a81acfb31675a468ef0b1881355e6e0fcc471
BJ Setiawan <bill.jodh@gmail.com>
2020-06-30 08:34:31 +0700

    first commit

---
+++ template/bootstrap_4/partials/footer.php
@@ -0,0 +1,18 @@
+<footer class="s-footer animated fadeInUp" id="hsr">
+    <div class="s-footer-content"><div class="row">
+        <div class="col-lg-12 col-sm-12 col-xs-12">
+            <div class="s-footer-tagline">
+            <a href="//slims.web.id" target="_blank"><?php echo SENAYAN_VERSION; ?>
+                </a>
+                    Dikelola Oleh Bagian Hukum Sekretariat Daerah Kabupaten Purbalingga
+                </div></div>
+                <!-- <div class="col-lg-3 col-sm-6 col-xs-12">
+                        <div class="s-menu-info"><form class="language" name="langSelect" action="index.php" method="get">
+                            <label class="language-info" for="select_lang">
+                                <?php echo __('Select Language'); ?></label> <span class="custom-dropdown custom-dropdown--white custom-dropdown--small">
+                                    <select name="select_lang" id="select_lang" title="Change language of this site" onchange="document.langSelect.submit();" class="custom-dropdown__select">
+                                     <?php echo $language_select; ?> </select> </span></form></div></div>
+                </ul> </nav>
+            </div> -->
+        </div>
+    </footer>
